f0e6e8e01527d49b3df8e4cdbbc876f999d7c31d
hanea-oz <hanaeamri12@gmail.com>
2025-12-08 19:47:46 +0000

    All done

--- app/app.py
+++ app/app.py
@@ -55,4 +55,5 @@ def open_browser():
 # ICI AUSSI : _name_ et "_main_" (deux underscores)
 if __name__ == "__main__":
     threading.Thread(target=open_browser).start()
+    app.run(host="127.0.0.1", port=5000, debug=True)
 
